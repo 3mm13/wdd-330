37b0e8df8841eb67f98c3f8689b1301042e8a3d1
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-04-28 15:02:02 -0600

    create function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 function populateList() {
-    document.querySelector("#list")
 
     const links = [
         {
@@ -12,6 +11,23 @@ function populateList() {
     for(let i = 0; i < links.length; i++){
         var li = document.createElement("li");
         li.setAttribute("a", i.url);
+        li.innerText = `Week${i + 1}`;
+        document.querySelector("#list").appendChild(li);
+        
     }
 
+}
+
+const links = [
+    {
+        label: "Week 1: Notes",
+        url: "week1/index.html"
+    }
+]
+
+for(let i = 0; i < links.length; i++){
+    var li = document.createElement("li");
+    li.innerHTML = `<a href="${links[i]["url"]}">Week0${i+1}</a>`
+    document.querySelector("#list").appendChild(li);
+    
 }
