0f1c23dcff46d23417da5b494027ee4cecb80f7b
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-04-30 21:59:41 -0600

    finalize team work documents

--- js/main.js
+++ js/main.js
@@ -5,29 +5,19 @@ function populateList() {
         {
             label: "Week 1: Notes",
             url: "week1/index.html"
+        },
+        {
+            label: "Week 1: Notes",
+            url: "week2/index.html"
         }
     ]
 
     for(let i = 0; i < links.length; i++){
         var li = document.createElement("li");
-        li.setAttribute("a", i.url);
-        li.innerText = `Week${i + 1}`;
+        li.innerHTML = `<a href="${links[i]["url"]}">Week0${i+1}</a>`
         document.querySelector("#list").appendChild(li);
         
     }
 
 }
 
-const links = [
-    {
-        label: "Week 1: Notes",
-        url: "week1/index.html"
-    }
-]
-
-for(let i = 0; i < links.length; i++){
-    var li = document.createElement("li");
-    li.innerHTML = `<a href="${links[i]["url"]}">Week0${i+1}</a>`
-    document.querySelector("#list").appendChild(li);
-    
-}
