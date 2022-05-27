782ef374363950e9ddfb319a11a930401a8415ef
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-05-27 17:38:23 -0600

    start todo

---
+++ week6/main.js
@@ -0,0 +1,10 @@
+
+function createTodoElement() {
+    var elem = document.createElement("li");
+    var inputValue = document.getElementById("submitTodo").value;
+    var text = document.createTextNode(inputValue);
+    elem.appendChild(text);
+    if (input == '') {
+        ""
+    }
+}
